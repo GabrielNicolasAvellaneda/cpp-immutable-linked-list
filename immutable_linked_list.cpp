99c876682113198653d8798d3055b5275a4b4497
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-12-13 12:07:09 -0300

    Node chaining refactoring.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- immutable_linked_list.cpp
+++ immutable_linked_list.cpp
@@ -4,17 +4,28 @@ using namespace std;
 
 struct Node {
 	int value;
-	Node* next = NULL;
+	Node* _next = NULL;
+
+	Node next() {
+		return (*_next);
+	}
+
+	Node next(Node* node) {
+		_next = node;
+		return next();
+	}	
 };
 
 int main() {
 
-	Node node1, node2;
+	Node node1, node2, node3;
 	node1.value = 1;
-	node1.next = (Node*)&node2;
+	node1.next(&node2);
 	node2.value = 2;
+	node3.value = 3;
+	node2.next(&node3);
 
-	cout << node1.value << " " << node2.value << " " << (*node1.next).value;
+	cout << node1.value << " " << node2.value << " " << node1.next().value << " " << node1.next().next().value << endl;
 
 	return 0;
 }
