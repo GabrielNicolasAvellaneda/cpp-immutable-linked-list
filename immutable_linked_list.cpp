eb6a062a9664e5e537b950442def9238cb2cdd46
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-12-13 13:24:43 -0300

    factory method passing an array of items.
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- immutable_linked_list.cpp
+++ immutable_linked_list.cpp
@@ -2,30 +2,56 @@
 #include <cstddef>
 using namespace std;
 
-struct Node {
-	int value;
-	Node* _next = NULL;
+class List {
+	struct Node {
+		int value;
+		Node* _next = NULL;
 
-	Node next() {
-		return (*_next);
-	}
+		Node next() {
+			return (*_next);
+		}
 
-	Node next(Node* node) {
-		_next = node;
-		return next();
-	}	
+		Node next(Node* node) {
+			_next = node;
+			return next();
+		}	
+	};
+
+	public:
+		static List* create(int values[], int n) {
+			Node *head = NULL;
+			for (int i = n-1; i >= 0; i--) {
+				Node node;
+				node.value = values[i];
+				if (head != NULL) {
+					node.next(head);
+					head = &node;
+				} else {
+					head = &node;
+				}
+			}
+			List* list = new List();
+			(*list).head(head);
+			return list;
+		}
+
+		Node* _head;
+
+		void head(Node* head) {
+			this->_head = head;
+		}
+
+		int head() {
+			return this->_head->value;
+		}
 };
 
 int main() {
 
-	Node node1, node2, node3;
-	node1.value = 1;
-	node1.next(&node2);
-	node2.value = 2;
-	node3.value = 3;
-	node2.next(&node3);
-
-	cout << node1.value << " " << node2.value << " " << node1.next().value << " " << node1.next().next().value << endl;
+	int numbers[] = {1, 2, 3, 4, 5};
+	List *list = List::create(numbers, 5);
+	int head = list->head();
+	cout << "Head value: " << head << endl;
 
 	return 0;
 }
