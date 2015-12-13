04c715ece47ae654804e127e10662175b4d18422
GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>
2015-12-13 11:55:06 -0300

    Node chaining
    Signed-off-by: GabrielNicolasAvellaneda <avellaneda.gabriel@gmail.com>

--- immutable_linked_list.cpp
+++ immutable_linked_list.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
+#include <cstddef>
 using namespace std;
 
 struct Node {
 	int value;
+	Node* next = NULL;
 };
-
 
 int main() {
 
-	Node node;
+	Node node1, node2;
+	node1.value = 1;
+	node1.next = (Node*)&node2;
+	node2.value = 2;
 
-	node.value = 1;
-
-	cout << node.value;
+	cout << node1.value << " " << node2.value << " " << (*node1.next).value;
 
 	return 0;
 }
